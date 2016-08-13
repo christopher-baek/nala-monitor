e491f03544c59a5be35b9cddd5aee85858df383c
Christopher Baek <christopher.baek@gmail.com>
2016-08-12 23:20:53 -0700

    Update getUserMedia and AudioContext instantiation
    I thought getUserMedia could be assigned as a function, but it seems like this can only be called as navigator.getUserMedia. Also added pause and resume calls on the AudioContext object on start and stop.

--- app/services/audio.js
+++ app/services/audio.js
@@ -1,23 +1,39 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+    started: false,
     init() {
         this._super(...arguments);
+        navigator.getUserMedia = (navigator.getUserMedia ||
+                                  navigator.webkitGetUserMedia ||
+                                  navigator.mozGetUserMedia ||
+                                  navigator.msGetUserMedia);
 
-        this.set('getUserMedia', (navigator.getUserMedia ||
-                                   navigator.webkitGetUserMedia ||
-                                   navigator.mozGetUserMedia ||
-                                   navigator.msGetUserMedia));
-        
-        this.set('audioContext', new (window.AudioContext || window.webkitAudioContext)());
+        this.set('audioContext', this._createAudioContext());
 
         // TODO: handle error scenarios if getUserMedia and audioContext
         // aren't available
     },
+    _createAudioContext() {
+        let AudioContext = (window.AudioContext || window.webkitAudioContext);
+        return new AudioContext();
+    },
     startListening() {
-        alert("start listening!");
+        if (this.get('started')) {
+            this.get('audoiContext').resume();
+        } else {
+            navigator.getUserMedia({audio: true}, this._handleStream, this._handleError);
+            this.set('started', true);
+        }
+    },
+    _handleStream(stream) {
+        let source = this.audioContext.createMediaStreamSource(stream);
+    },
+    _handleError(error) {
+        alert("Received the follownig error: " + error);
+        // TODO: notify the context object of failure
     },
     stopListening() {
-        alert("stop listening!");
+        this.get('audioContext').pause();
     }
 });
